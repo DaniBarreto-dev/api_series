319e9a881109aa3b2eebc49e0fb066c6880e46fe
DaniBarreto-dev <danibdepaula@gmail.com>
2026-03-09 16:42:39 -0300

    Finalizando a aula

--- api_series/main.py
+++ api_series/main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/itens/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/soma")
+def soma(a : int, b:int):
+    return{"resultado": a + b}
+
+@app.get("/soma/{a}/{b}")
+def soma(a : int, b:int):
+    return{"resultado": a + b}
